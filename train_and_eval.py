50fd544cdca9dd15260931a1c838f1cb39bc713c
VicenteVivan <vicente.vivancoc@gmail.com>
2022-07-17 17:22:32 -0400

    Switch Labels

--- train_and_eval.py
+++ train_and_eval.py
@@ -65,9 +65,9 @@ def train(train_dataloader, model, model_name, criterion, optimizer, opt, epoch,
         
         # Randomly switch 10% of the labels (Torch)
         should_switch = torch.rand(batch_size) < 0.1
-        y = y.detach().cpu().numpy()
-        y[should_switch] = torch.randint(0, 10, (np.sum(should_switch))).long()
-        y = torch.from_numpy(y).long()
+        y = y.detach().long()
+        y[should_switch] = torch.randint(0, 10, (torch.sum(should_switch))).long()
+        y = y.long()
 
         loss = criterion(preds, y)
         loss.backward()
