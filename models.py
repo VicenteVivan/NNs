0f98d46b7a04a2de51a96d2b9203a2988e22c5b5
VicenteVivan <vicente.vivancoc@gmail.com>
2022-07-22 10:09:59 -0400

    Cifar 100

--- models.py
+++ models.py
@@ -30,41 +30,40 @@ def getNetwork(input_size, output_size, num_hidden_layers, hidden_layer_size):
         network.add_module("hidden" + str(i), nn.Linear(hidden_layer_size, hidden_layer_size))
         network.add_module("relu" + str(i), nn.ReLU())
     network.add_module("output", nn.Linear(hidden_layer_size, output_size))
-    network.add_module("softmax", nn.Softmax(dim=1))
     return network
 
 # input_size = 1568
 input_size = 2048
 
 name1 = "(i = 1): 46555"
-net1 = getNetwork(input_size, 10, 1, 46555)
+net1 = getNetwork(input_size, 100, 1, 46555)
 
 name2 = "(i = 2): 8984"
-net2 = getNetwork(input_size, 10, 2, 8984)
+net2 = getNetwork(input_size, 100, 2, 8984)
 
 name3 = "(i = 3): 6554"
-net3 = getNetwork(input_size, 10, 3, 6554)
+net3 = getNetwork(input_size, 100, 3, 6554)
 
 name4 = "(i = 4): 5427"
-net4 = getNetwork(input_size, 10, 4, 5427)
+net4 = getNetwork(input_size, 100, 4, 5427)
 
 name5 = "(i = 5): 4739"
-net5 = getNetwork(input_size, 10, 5, 4739)
+net5 = getNetwork(input_size, 100, 5, 4739)
 
 name6 = "(i = 6): 4262"
-net6 = getNetwork(input_size, 10, 6, 4262)
+net6 = getNetwork(input_size, 100, 6, 4262)
 
 name7="(i = 7): 3907"
-net7 = getNetwork(input_size, 10, 7, 3907)
+net7 = getNetwork(input_size, 100, 7, 3907)
 
 name8="(i = 8): 3629"
-net8 = getNetwork(input_size, 10, 8, 3629)
+net8 = getNetwork(input_size, 100, 8, 3629)
 
 name9="(i = 9): 3404"
-net9 = getNetwork(input_size, 10, 9, 3404)
+net9 = getNetwork(input_size, 100, 9, 3404)
 
 name10="(i = 10): 3216"
-net10 = getNetwork(input_size, 10, 10, 3216)
+net10 = getNetwork(input_size, 100, 10, 3216)
 
 def getModels():
 
