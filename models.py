0984c6917c559f5079d3696cc487e07470c01c22
Vicente Vivanco Cepeda <vicentevivanco@knights.ucf.edu>
2022-08-20 13:52:59 -0400

    256

--- models.py
+++ models.py
@@ -95,16 +95,13 @@ net7 = getNetwork(input_size, c, 65, L[6])
 name8 = f'(i = 128): {L[7]}'
 net8 = getNetwork(input_size, c, 129, L[7])
 
-name9 = f'(i = 256): {L[8]}'
-net9 = getNetwork(input_size, c, 257, L[8])
-
 def getModels():
-    return [net9, net8, net7, net6, net5, net4, net3, net2, net1]
+    return [net8, net7, net6, net5, net4, net3, net2, net1]
     #return [net1, net2, net3, net4, net5, net6, net7, net8, net9, net10]
 
 
 def getNames():
-    return [name9, name8, name7, name6, name5, name4, name3, name2, name1]
+    return [name8, name7, name6, name5, name4, name3, name2, name1]
     #return [name1, name2, name3, name4, name5, name6, name7, name8, name9, name10]
 
 if __name__ == "__main__":
